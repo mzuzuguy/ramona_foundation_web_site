38d6827fc6c478caaf943c397ee12bf024eeaa8c
mzuzuguy <preciousmusole18@gmail.com>
2026-05-16 23:51:45 +0200

    did the role-controller

--- ramona/src/role/role.module.ts
+++ ramona/src/role/role.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
 import { RoleService } from './role.service';
+import { RoleController } from './role.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Role } from './entities/role.entity';
+import { User } from '../user/entities/user.entity';
 
 @Module({
-  providers: [RoleService]
+  imports: [TypeOrmModule.forFeature([Role, User])],
+  controllers:[RoleController],
+  providers: [RoleService],
+  exports: [RoleService], 
 })
 export class RoleModule {}
